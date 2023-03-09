0ab4617527f85e9068d46b4fc44feaf7a56f967d
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-03-08 21:48:14 -0300

    Grande Parte feita da Página Principal

---
+++ UC12/Projeto Integrador/js/script.js
@@ -0,0 +1,10 @@
+// var tagMAIN = document.querySelector("main");
+// var calendario = "<div class=\"flexColumn\"></div>";
+// for(let cont = 1; cont<=4; cont++){
+//     calendario += "<div class=\"linhas flexRow\">";
+//     for(let cont2 = 1; cont2<=7; cont2++){
+//         calendario += "<div class=\"dias\">"+cont*cont2+"</div>";
+//     }
+//     calendario +="</div>";
+// }
+// tagMAIN.innerHTML+="<div>"+calendario+"</div>";
