7b4ba2c09ed570d9445a45374f41a3b124e77ba9
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-03-13 21:55:14 -0300

    continuando site PI - SENAC

--- UC12/Projeto Integrador/js/script.js
+++ UC12/Projeto Integrador/js/script.js
@@ -7,4 +7,17 @@
 //     }
 //     calendario +="</div>";
 // }
-// tagMAIN.innerHTML+="<div>"+calendario+"</div>";
+// tagMAIN.innerHTML+="<div>"+calendario+"</div>";]
+
+// function dropDowns_ToogleNone(){
+//     var allDropDownsAtivos = document.querySelectorAll(".visivel");
+//     allDropDownsAtivos.forEach(dd => {
+//         dd.classList.remove("visivel"); 
+//         dd.classList.add("invisivel");
+//     });
+// }
+// function menu_dropDown(dropDownClass){
+//     var dropdown = document.querySelector("."+dropDownClass);
+//     dropdown.classList.remove("invisivel");
+//     dropdown.classList.add("visivel");
+// }
