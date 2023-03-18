5dbb5f4b91fa85faeefc6f3b3a3e4cc60f7995fb
Diego Rafael Maciel De Moraes <887119@senacminas.edu.br>
2023-03-17 21:48:18 -0300

    mais partes do site PI - SENAC

--- UC12/Projeto Integrador/js/script.js
+++ UC12/Projeto Integrador/js/script.js
@@ -1,23 +1,23 @@
-// var tagMAIN = document.querySelector("main");
-// var calendario = "<div class=\"flexColumn\"></div>";
-// for(let cont = 1; cont<=4; cont++){
-//     calendario += "<div class=\"linhas flexRow\">";
-//     for(let cont2 = 1; cont2<=7; cont2++){
-//         calendario += "<div class=\"dias\">"+cont*cont2+"</div>";
+// function scrollToFirstText(formulario){
+//     var valor_busca = formulario.querySelector('input').value;
+//     alert(valor_busca);
+//     var secoesList = document.body.querySelectorAll('main section');
+//     alert(secoesList);
+//     alert(secoesList[0].textContent);
+//     for(let cont = 0; cont< secoesList; cont++){
+//         var lista=[];
+//         let cont2 = 0;
+//         let trimSection = sections[0].textContent.trim().split("\n");
+//         for(let cont = 0;cont<trimSection.length;cont++){
+//             let valor = trimSection[cont].trim();
+//             if(valor!=""){
+//                 lista[cont2]=valor; cont2++;
+//             }
+//         }
+//         if (lista[cont].textContent.(valor_busca)){
+//             alert('FOUND IT!!!!! NA SECTION: '+(cont+1));
+//         } else {
+//             alert('NADA NA SEÇÃO: '+(cont+1));
+//         }
 //     }
-//     calendario +="</div>";
-// }
-// tagMAIN.innerHTML+="<div>"+calendario+"</div>";]
-
-// function dropDowns_ToogleNone(){
-//     var allDropDownsAtivos = document.querySelectorAll(".visivel");
-//     allDropDownsAtivos.forEach(dd => {
-//         dd.classList.remove("visivel"); 
-//         dd.classList.add("invisivel");
-//     });
-// }
-// function menu_dropDown(dropDownClass){
-//     var dropdown = document.querySelector("."+dropDownClass);
-//     dropdown.classList.remove("invisivel");
-//     dropdown.classList.add("visivel");
 // }
